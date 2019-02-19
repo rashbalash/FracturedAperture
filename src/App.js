e7fec4339c3da16b6efabe0cbd4b501b97c9cc74
Rashad <Rashad.Balashov@gmail.com>
2019-02-19 16:45:21 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div id="mainTitle">
+          <p id="title">Fractured Aperture</p>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
